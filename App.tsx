2698d0cc5aa37c0b07eb51adf0a2f0e3b88d40f2
dmitryou <dmitryourkevich@gmail.com>
2025-04-07 16:26:27 -0600

    weather app

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
+import type { PropsWithChildren } from 'react';
+import axios from 'axios';
 import {
   ScrollView,
   StatusBar,
@@ -14,21 +15,34 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
+  TextInput,
+  Image,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
   Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+
+interface WeatherParams {
+  location: string;
+  image: string;
+}
+
+
+function extractWeatherData(response: any) {
+  const location = response?.data?.location?.name;
+  const image = response?.data?.current?.weather_icons[0];
+  return { location, image };
+}
+
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -61,15 +75,41 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
+
+  const [city, setCity] = useState<any>(null);
+  const [data, setData] = useState<any>(null);
+  const [isLoading, setIsloading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+
+  const getWeather = async () => {
+    if (!city.trim()) return; // Avoid making a call if input is empty
+    try {
+      setIsloading(true);
+      const response = await axios.get(`https://api.weatherstack.com/current`, {
+        params: {
+          access_key: 'd5270de54dcf234837ea0468df6af0ab',
+          query: city
+        }
+      });
+      const weatherData: WeatherParams = extractWeatherData(response);
+      setData(weatherData);
+      setIsloading(false);
+
+      setError(null); // Clear any previous errors
+    } catch (err) {
+      setIsloading(false);
+      setError('An error occurred while fetching data.');
+      setData(null); // Clear any previous data
+    }
+  };
+
+  const onCityChange = (text: string) => {
+    console.log('text', text);
+    setCity(text);
+    console.log('city', city);
+  }
+
   const safePadding = '5%';
 
   return (
@@ -80,32 +120,48 @@ function App(): React.JSX.Element {
       />
       <ScrollView
         style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+        <View style={{ paddingRight: safePadding }}>
+          <Header />
         </View>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
             paddingHorizontal: safePadding,
             paddingBottom: safePadding,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Get weather in any city">
+            Enter city name and press weather for this city.
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
-      </ScrollView>
-    </View>
+        {isLoading === true ? <View style={styles.container}>
+          <Text style={styles.highlight}>{'Loading data'}</Text>
+        </View> : error ? <View style={styles.container}>
+          <Text style={styles.highlight}>{error}</Text>
+        </View> :
+          <>
+            <TextInput
+              style={styles.input}
+              onChangeText={(e) => { onCityChange(e) }}
+              value={city}
+            />
+            <Button
+              onPress={getWeather}
+              title="Get Weather"
+              accessibilityLabel="Learn more about this purple button"
+            />
+            <View style={styles.container}>
+              <Text style={styles.highlight}>{data?.location}</Text>
+            </View>
+
+            <View style={styles.container}>
+              <Image
+                style={styles.tinyLogo}
+                source={{
+                  uri: data?.image,
+                }}
+              />
+            </View></>}
+      </ScrollView >
+    </View >
   );
 }
 
@@ -126,6 +182,22 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flex: 1,
+    marginTop: 20,
+  }
 });
 
 export default App;
